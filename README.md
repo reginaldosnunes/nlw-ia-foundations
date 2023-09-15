d2f416360f69b18f491d3d969113ddb6f0b5b444
Reginaldo Nunes <contato@reginaldonunes.com.br>
2023-09-15 06:35:04 -0300

    new file:   .github/cover.png 	modified:   .gitignore 	new file:   README.md 	modified:   package-lock.json 	modified:   package.json 	new file:   server/convert.js 	modified:   server/index.js 	modified:   server/summarize.js 	modified:   server/transcribe.js 	deleted:    tmp/audio.mp4 	modified:   web/form.js

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <img  src=".github/cover.png" alt="UI do projeto" width="100%">
+</p>
+
+## 🤖 Shorts-Summary
+Projeto utilizando modelos de IA para gerar resumo de Shorts do Youtube.
+
+Desenvolvido durante o NLW IA da Rocketseat.
+
+## 🛠 Tecnologias 
+
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- IA
+
+## 🎨 Layout
+Layout figma disponível em [link](https://www.figma.com/community/file/1282823495335498952).
--- web/form.js
+++ web/form.js
@@ -6,6 +6,7 @@ const content = document.querySelector("#content")
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault()
+    content.classList.add("placeholder")
 
     const videoURL = input.value
 
@@ -27,4 +28,9 @@ form.addEventListener("submit", async (event) => {
     })
 
     content.textContent = summary.data.result
+    content.classList.remove("placeholder")
+
+    //Full transcription
+    /* content.textContent = transcription.data.result
+    content.classList.remove("placeholder") */
 })
